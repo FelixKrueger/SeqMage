5f6f50bfe1739f32b9cb390f5b638a7ee32581a7
Felix Krueger <felix.krueger@babraham.ac.uk>
2017-12-06 09:52:21 +0000

    First real commit

---
+++ uk/ac/babraham/Reader/Reader.java
@@ -0,0 +1,10 @@
+package uk.ac.babraham.Reader;
+
+public class Reader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Getting started");
+	}
+
+}
